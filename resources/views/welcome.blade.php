d164234d495d2ea7c6db1dd277ca29408c3c9773
yasminrazali <yasminrazali@gmail.com>
2020-03-12 15:12:14 +0800

    Change the text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel I change here
                 </div>
 
                 <div class="links">
